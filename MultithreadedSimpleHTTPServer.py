bf994ab7203cffd7b06cd1c5b91b6216357cbc46
Michelfrancis Bustillos <michelfrancisb@gmail.com>
2018-01-03 18:44:10 -0500

    Added I/O with Error Checking
    Added I/O in case command line arguements were not passed. Added input validation for command line arguments as well as user input.

--- MultithreadedSimpleHTTPServer.py
+++ MultithreadedSimpleHTTPServer.py
@@ -1,33 +1,60 @@
 #!/bin/env python
 
 try:
-    # Python 2.x
-    from SocketServer import ThreadingMixIn
-    from SimpleHTTPServer import SimpleHTTPRequestHandler
-    from BaseHTTPServer import HTTPServer
+	# Python 2.x
+	from SocketServer import ThreadingMixIn
+	from SimpleHTTPServer import SimpleHTTPRequestHandler
+	from BaseHTTPServer import HTTPServer
 except ImportError:
-    # Python 3.x
-    from socketserver import ThreadingMixIn
-    from http.server import SimpleHTTPRequestHandler, HTTPServer
+	# Python 3.x
+	from socketserver import ThreadingMixIn
+	from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
-    pass
+	pass
 
 import sys
 import os
 
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 8000
+port = ''
 
+if sys.argv[1:]:
+	port = sys.argv[1]
+	
+if not port.isdigit():
+	while True:
+		try:
+			port = int(input("Enter port (default = 80): "))
+		except ValueError:
+			print("INVALID PORT")
+			continue
+		else:
+			break
+
+port = int(port)
+directory = ''
 if sys.argv[2:]:
-    os.chdir(sys.argv[2])
+	directory = sys.argv[2]
+	
+if not os.path.isdir(directory):
+	directory = input("Enter directory to start server: ")
+
+while not os.path.isdir(directory):
+		print("INVALID DIRECTORY!")
+		directory = input("Enter directory to start server: ")
+
+os.chdir(directory)
+directory = os.getcwd()
+
+print(“\033[H\033[J”)
+print ('Server started on port %s at %s' % (port, directory))
+print ('Close Command Window to end.')
 
 server = ThreadingSimpleServer(('', port), SimpleHTTPRequestHandler)
 try:
-    while 1:
-        sys.stdout.flush()
-        server.handle_request()
+	while 1:
+		sys.stdout.flush()
+		server.handle_request()
 except KeyboardInterrupt:
-    print("Finished")
+	print("Finished")
+	
